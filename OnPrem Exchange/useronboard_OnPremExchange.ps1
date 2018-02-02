8c5b4a40a06694391ecfa8f7e1375e379cca5532
Christopher Morris <morris.christopher@gmail.com>
2018-02-01 16:42:53 -0800

    Added Get-Database
    Added search to get the Mailbox Databases from the exchange server, and print out the Database. Allowing the user to select, and add a user to the specific database.

--- OnPrem Exchange/useronboard_OnPremExchange.ps1
+++ OnPrem Exchange/useronboard_OnPremExchange.ps1
@@ -48,7 +48,17 @@ Write-Host "*********************************************"
 Write-Host "Creating mailbox" -ForegroundColor Yellow -BackgroundColor DarkGreen
 Write-Host "*********************************************"
 
-Enable-mailbox -identity $UPN
+#Get Mailbox Database
+$Databaselist = Get-MailboxDatabase  | select Name
+$linecounter = 1
+   foreach($Databasename in $Databaselist){
+      Write-Host($linecounter.ToString() + ". " + $Databasename.Name)
+      $linecounter++
+    }
+Write-host "`r`n"
+$Database = $Databaselist[[int](Read-Host -Prompt "Please enter the number of the Mailbox Database you wish to place this new user in (i.e. 1 or 5)")-1]
+
+Enable-mailbox -identity $UPN -Database $Database
 
 Write-Host "*********************************************"
 Write-Host "Mailbox created!" -ForegroundColor Yellow -BackgroundColor DarkGreen
